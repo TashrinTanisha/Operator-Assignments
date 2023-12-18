588b382e4441000a33a4cd00c2a66ad8717ddb40
TashrinTanisha <145369121+TashrinTanisha@users.noreply.github.com>
2023-12-18 13:03:52 +0200

    Add files via upload

---
+++ Tanisha-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    //Area of square
+
+    //Take two variables called side and area
+
+    int side,area;
+
+    //Input the side
+
+    printf("Enter side :- ");
+    scanf("%d",&side);
+
+    //Calculate the area of square
+
+    area = side * side;
+
+    //At last,print the area
+
+    printf("Area of square is :- %d",area);
+
+    return 0;
+}
