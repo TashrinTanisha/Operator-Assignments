db4c32fe277290111a0d12b8442d1cd527ee4230
Tashrin Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-05-03 23:07:42 +0600

    Arithmetic Operator

---
+++ Array-16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num[5];
+
+    num[1] = 10 ;
+
+    num[3] = 50 ;
+
+    int sum = num[1] + num[3] ;
+
+    printf("Sum is : %d\n", sum) ;
+
+    return 0 ;
+}
