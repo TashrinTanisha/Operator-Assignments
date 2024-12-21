f2a91b9b44cfb316a2e0f79ffbf0b0e590606b8d
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-12-22 01:30:04 +0600

    Arithmetic, Assignment Operator & Comma Operators

---
+++ Addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2;
+
+    printf("Enter num1 : ");
+    scanf("%d", &num1);
+
+    printf("Enter num2 : ");
+    scanf("%d", &num2);
+
+    int sum = num1 + num2 ;
+
+    printf("The sum is : %d", sum);
+
+    return 0;
+}
