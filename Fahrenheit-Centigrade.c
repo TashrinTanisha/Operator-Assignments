cf7cf3e5f5b8b7a2df23abed2226a50f2b6f265b
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-12-25 22:30:49 +0600

    Assignment Operator & Arithmetic Operators

---
+++ Fahrenheit-Centigrade.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+[
+    float c,F;
+
+    printf("Enter fahrenheit : ");
+    scanf("%f",&F);
+
+    c = (F-32)/1.8;
+
+    printf("Centigrade : %f",c);
+
+    return 0;
+]
