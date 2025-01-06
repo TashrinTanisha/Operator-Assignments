9fb6a43066118cee78e6cd4c46a8063b5cf6b129
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2025-01-06 23:52:27 +0600

    Arithmetic & Relational Operator

---
+++ If Else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int integer;
+
+    printf("Enter integer : ");
+    scanf("%d",&integer);
+
+    if(integer % 2 == 0){
+        printf("Even\n");
+    }else{
+        printf("Odd");
+    }
+
+    return 0;
+}
