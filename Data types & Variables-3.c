67a81666221e34a1c7103de4409641f8ac59d7cd
Tashrin Tarannum Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2025-01-11 19:58:19 +0600

    Assignment Operators

---
+++ Data types & Variables-3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char Student1 = 'A', Student2 = 'F';
+
+    printf("Student1 grade is: %c\n", Student1);
+    printf("Student2 grade is: %c\n", Student2);
+
+    return 0;
+}
