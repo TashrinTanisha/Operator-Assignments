55752b9ffdd70e26d8d5263f4b9cbd7363314569
Tashrin Tarannum Tanisha <145369121+TashrinTanisha@users.noreply.github.com>
2025-08-04 22:13:48 +0600

    Assignment operators

---
+++ Character.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char my_name1, my_name2, my_name3, my_name4;
+
+    my_name1 = 'T';
+    my_name2 = 'a';
+    my_name3 = 'n';
+    my_name4 = 'i';
+
+    printf("%c%c%c%c\n", my_name1, my_name2, my_name3, my_name4);
+
+    return 0;
+}
