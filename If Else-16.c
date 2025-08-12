c887a15cb0d0d95e9a417040375fd7bc2bebde60
Tashrin Tarannum Tanisha <145369121+TashrinTanisha@users.noreply.github.com>
2025-08-12 20:43:51 +0600

    Assignment, relational & logical operator

---
+++ If Else-16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int y = 25;
+
+    if(y > 10 && y < 100)
+    {
+        printf("Condition is true\n");
+        printf("It's working!\n");
+    }
+
+    return 0;
+}
