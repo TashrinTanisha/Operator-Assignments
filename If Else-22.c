2c362fbcbd4057d9e5ecd161b930535237962b37
Tashrin Tarannum Tanisha <145369121+TashrinTanisha@users.noreply.github.com>
2025-08-15 22:50:52 +0600

    Assignment & relational operator

---
+++ If Else-22.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int ticket_price = 200;
+
+    if(ticket_price <= 200)
+    {
+
+        printf("I will go\n");
+
+    }
+    else
+    {
+
+        printf("I won't go\n");
+
+    }
+
+    return 0;
+}
