9c4a51a2561a4d663db274cde9540050b82b25a2
Tashrin Tarannum Tanisha <145369121+TashrinTanisha@users.noreply.github.com>
2025-08-15 22:51:37 +0600

    Arithmetic, relational & logical operators

---
+++ If Else-23.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+
+    printf("Enter the value of x: ");
+    scanf("%d", &x);
+
+    if(x % 3 == 0 || x % 5 == 0)
+    {
+
+        printf("The number is divisible by 3 or 5\n ");
+
+    }
+    else
+    {
+
+        printf("The number is not divisible by 3 or 5\n");
+
+    }
+
+    return 0;
+}
